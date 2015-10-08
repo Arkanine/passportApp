22dfc47dd6a1a09e347981756fcb3f34b9a25538
roman <rozabi@pascalium.com>
2015-10-08 15:30:42 +0300

    swagger implemented

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var express      = require('express'),
     morgan       = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser   = require('body-parser'),
-    session      = require('express-session');
+    session      = require('express-session'),
+
+    argv         = require('minimist')(process.argv.slice(2)),
+    swagger      = require("swagger-node-express");
 
 var app          = express(),
     server       = require('http').Server(app),
     io           = require('socket.io')(server);
-
 
 // Connect to mongodb
 var configDB = require('./config/database.js');
@@ -33,8 +35,56 @@ app.use(session({ secret: 'secretword' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./app/routes')(app, passport);
+/**
+ * Swagger
+ */
+var models  = require('./swagger/models/models'),
+    test    = require('./swagger/models/test');
 
-var port = process.env.PORT || 8080;
+app.use('/js', express.static('swagger/js'));
+app.use('/lib', express.static('swagger/lib'));
+app.use('/css', express.static('swagger/css'));
+app.use('/images', express.static('swagger/images'));
+// Set the main handler in swagger to the express app
+swagger.setAppHandler(app);
+// Adding models and methods to our RESTFul service
+swagger.addModels(models)
+    .addGet(test.dummyTestMethod);
+// set api info
+swagger.setApiInfo({
+    title: "KnowlegeBS API",
+    description: "API to manage job applications",
+    termsOfServiceUrl: "",
+    contact: "rozabi@pascalium.com",
+    license: "",
+    licenseUrl: ""
+});
+
+app.get('/api-doc', function (req, res) {
+    res.sendfile('swagger/swagger.html');
+});
+
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".');
+
+var port = 8080;
+if(argv.port !== undefined)
+    port = argv.port;
+else
+    console.log('No --port=xxx specified, taking default port ' + port + '.');
+
+var applicationUrl = 'http://' + domain + ':' + port;
+console.log('Knowlege BS API running on ' + applicationUrl);
+
+swagger.configure(applicationUrl, '1.0.0');
+
+require('./app/routes')(app, passport); // password injection
+
+//var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Port: ' + port);
