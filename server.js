f0c54a11855c6912ac9af033492a528377b8e852
roman <rozabi@pascalium.com>
2015-10-08 12:07:57 +0300

    prepear to integrate swagger

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express      = require('express'),
-    favicon      = require('static-favicon'),
+    favicon      = require('serve-favicon'),
     mongoose     = require('mongoose'),
     passport     = require('passport'),
     flash        = require('connect-flash'),
@@ -22,27 +22,19 @@ connect();
 mongoose.connection.on('error', console.log);
 mongoose.connection.on('disconnected', connect);
 
-//var stripe = require("stripe")("sk_test_72FX8WSRUD9ojDFW2jkzfBrr");
-//var stripeToken = request.body.stripeToken;
-//console.log(stripeToken);
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(flash());
+app.set('view engine', 'jade');
 
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-app.use(favicon());
-app.use(flash()); // use connect-flash for flash messages stored in session
-app.set('view engine', 'jade'); // set up jade for templating
-
-// required for passport
-require('./config/passport')(passport); // pass passport for configuration
-app.use(session({ secret: 'secretword' })); // session secret
+require('./config/passport')(passport);
+app.use(session({ secret: 'secretword' }));
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+app.use(passport.session());
 
-// routes ======================================================================
-require('./app/routes')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes')(app, passport);
 
-// launch ======================================================================
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Port: ' + port);
